feed62273f3b49d5afb05e05c1037bd5d5a6c105
Speed3xz 🎀 <211885527+speed3xz@users.noreply.github.com>
2025-10-10 17:16:08 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arlette-Bot • Powered by Speed3xz</title>
+    <script>
+        // Redirige automáticamente cuando se carga la página
+        window.onload = function() {
+            window.location.href = 'https://whatsapp.com/channel/0029VbAmwbQBqbr587Zkni1a'; // Aquí pones el enlace al que deseas redirigir
+        };
+    </script>
+</head>
+<body>
+    <!-- El contenido de esta página no se muestra debido a la redirección automática -->
+</body>
+</html>
